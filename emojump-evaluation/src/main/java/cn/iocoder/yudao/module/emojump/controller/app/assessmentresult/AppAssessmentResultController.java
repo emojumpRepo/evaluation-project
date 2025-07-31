a5b3c33ec53becdbac59d998c5f8ca887256a05c
Swance94 <15876597212@163.com>
2025-07-31 15:58:50 +0800

    feat(emojump): 添加@Valid注解以验证请求参数

--- emojump-evaluation/src/main/java/cn/iocoder/yudao/module/emojump/controller/app/assessmentresult/AppAssessmentResultController.java
+++ emojump-evaluation/src/main/java/cn/iocoder/yudao/module/emojump/controller/app/assessmentresult/AppAssessmentResultController.java
@@ -11,6 +11,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 import javax.annotation.Resource;
 import javax.annotation.security.PermitAll;
 
