d8b3ed8e5de123cd158559452ea7b9f3ce9ce327
Luke Boxwell <130121308+catdddmouse@users.noreply.github.com>
2025-10-22 10:31:07 +0800

    feat(emojump): 添加文章分类表及更新文章表结构，支持分类ID

--- sql/mysql/emojump-article.sql
+++ sql/mysql/emojump-article.sql
@@ -1,4 +1,22 @@
 -- ----------------------------
+-- Table structure for emojump_article_category
+-- ----------------------------
+DROP TABLE IF EXISTS `emojump_article_category`;
+CREATE TABLE `emojump_article_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '分类ID',
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '分类名称',
+  `sort` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
+  `creator` varchar(64) NOT NULL DEFAULT '' COMMENT '创建者',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updater` varchar(64) NOT NULL DEFAULT '' COMMENT '更新者',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
+  `tenant_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '租户ID',
+  PRIMARY KEY (`id`),
+  KEY `idx_sort` (`sort`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章分类表';
+
+-- ----------------------------
 -- Table structure for emojump_article
 -- ----------------------------
 DROP TABLE IF EXISTS `emojump_article`;
@@ -7,7 +25,7 @@ CREATE TABLE `emojump_article` (
   `title` varchar(255) NOT NULL DEFAULT '' COMMENT '文章标题',
   `content` longtext COMMENT '文章内容',
   `cover_image` varchar(255) DEFAULT '' COMMENT '封面图片',
-  `category` varchar(50) DEFAULT '' COMMENT '文章分类',
+  `category_id` bigint(20) DEFAULT NULL COMMENT '文章分类ID',
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '文章状态',
   `view_count` int(11) NOT NULL DEFAULT '0' COMMENT '浏览次数',
   `like_count` int(11) NOT NULL DEFAULT '0' COMMENT '点赞次数',
@@ -20,6 +38,7 @@ CREATE TABLE `emojump_article` (
   `tenant_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '租户ID',
   `publish_time` bigint(20) DEFAULT NULL COMMENT '发布时间',
   PRIMARY KEY (`id`),
+  KEY `idx_category_id` (`category_id`),
   KEY `idx_status` (`status`),
   KEY `idx_create_time` (`create_time`),
   KEY `idx_publish_time` (`publish_time`)
